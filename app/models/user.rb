95b9c05e55329be52d3fecce31e7e77f31889bf2
mahmoud717 <mahmoudmohammad717@gmail.com>
2020-11-03 16:20:25 +0200

    finished configuring routes and linkes

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,17 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :friendships
   has_many :friendship_requests, class_name: "Friendship", foreign_key: "friend_id"
+  def friends
+    friends_array = friendships.map{|friendship| friendship.friend if friendship.confirmed} 
+    friends_array.compact
+  end
+  def pending_friends
+    friendships.map{|friendship| friendship.friend if !friendship.confirmed}.compact
+  end
+  def friend_requests
+    friendship_requests.map{|friendship| friendship.user if !friendship.confirmed}.compact
+  end
+  def friend?(user)
+    friends.include?(user)
+  end
 end
