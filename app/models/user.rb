71aa097f635c9c0ec835972f060c30006a5f889e
mahmoud717 <mahmoudmohammad717@gmail.com>
2020-11-04 09:08:16 +0200

    edited rubocop errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,17 +10,20 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :friendships
-  has_many :friendship_requests, class_name: "Friendship", foreign_key: "friend_id"
+  has_many :friendship_requests, class_name: 'Friendship', foreign_key: 'friend_id'
   def friends
-    friends_array = friendships.map{|friendship| friendship.friend if friendship.confirmed} 
+    friends_array = friendships.map { |friendship| friendship.friend if friendship.confirmed }
     friends_array.compact
   end
+
   def pending_friends
-    friendships.map{|friendship| friendship.friend if !friendship.confirmed}.compact
+    friendships.map { |friendship| friendship.friend unless friendship.confirmed }.compact
   end
+
   def friend_requests
-    friendship_requests.map{|friendship| friendship.user if !friendship.confirmed}.compact
+    friendship_requests.map { |friendship| friendship.user unless friendship.confirmed }.compact
   end
+
   def friend?(user)
     friends.include?(user)
   end
